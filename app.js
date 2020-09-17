b8cef9c8440f2b798d0640d3d52874416a686b0f
ducdunghvtc <69515593+ducdunghvtc@users.noreply.github.com>
2020-09-17 14:32:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,11 @@
 //  Một đối tượng có thể được định nghĩa là một trường dữ liệu có các thuộc tính và hành vi duy nhất.
 // 4 tính chất 
 // 
-// Có 4 tính chất chính tạo nên một ngôn ngữ Hướng đối tượng. Đó là Đóng gói,Trừu tượng hóa dữ liệu, Đa hình và Kế thừa.
+// Có 4 tính chất chính tạo nên một ngôn ngữ Hướng đối tượng: 
+// Đóng gói:Là cách để che dấu những tính chất xử lý bên trong của đối tượng, những đối tượng khác không thể tác động trực tiếp làm thay đổi trạng thái chỉ có thể tác động thông qua các method public của đối tượng đó.
+// Trừu tượng hóa dữ liệu:Là phương pháp trừu tượng hóa định nghĩa lên những hành động, tính chất của loại đối tượng nào đó cần phải có.
+// Đa hình:Là một đối tượng thuộc các lớp khác nhau có thể hiểu cùng một thông điệp theo cách khác nhau
+// Kế thừa:Là kỹ thuật cho phép kế thừa lại những tính năng mà một đối tượng khác đã có, giúp tránh việc code lặp dư thừa mà chỉ xử lý công việc tương tự
 //  Đây cũng được gọi là bốn trụ cột của Lập trình hướng đối tượng.
 
 
