4d1290f576c38633caba69f94b686edba8a158cf
ducdunghvtc <69515593+ducdunghvtc@users.noreply.github.com>
2020-09-29 19:33:17 +0700

    Update book.js

--- BTVN4/js/book.js
+++ BTVN4/js/book.js
@@ -1,11 +1,12 @@
-import { BookCase } from "./bookcase.js"
-
-export class Book extends BookCase{
+export class Book{
+    id
+    books_name
     author
     category
     dateModified
-    constructor(id,author,category) {
-        super(id)
+    constructor(id,books_name,author,category) {
+        this.id = id
+        this.books_name = books_name
         this.author = author
         this.category = category
         this.dateModified = new Date().toISOString()
